850897430a60e2f0508c081b2dd006cf5ebbc3e6
microscopy-Core-ISMMS <75392064+microscopy-Core-ISMMS@users.noreply.github.com>
2026-08-17 20:19:37 +0000

    added toc to the Laser Power Measurements page index.qmd

--- scripts/generate_microscope_pages.py
+++ scripts/generate_microscope_pages.py
@@ -98,7 +98,7 @@ index_lines = [
     "toc: true",
     "---",
     "",
-    "**Illumination Power Stability**",
+    "## Illumination Power Stability",
     "",
     "Quality assurance of illumination power stability is critical because "
     "fluorescence intensity measurements depend directly on the excitation "
@@ -120,7 +120,7 @@ index_lines = [
     "stability allows early detection of such fluctuations and helps ensure "
     "that excitation conditions remain consistent and reproducible.",
     "",
-    "**Confocal Microscopes Laser-power Quality Assurance Results**",
+    "## Confocal Microscopes Laser-power Quality Assurance Results",
     "",
     "Select a confocal microscope to view its laser-power quality assurance results.",
     "",
